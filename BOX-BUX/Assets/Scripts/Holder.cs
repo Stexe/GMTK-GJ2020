d0a8631a61a0782bca650c3537400c13b11261c8
Adam Reifsneider <adamreifsneider@gmail.com>
2020-07-11 08:51:21 -0400

    can pick up and drop Pickables
    must add pickable script to object and put in interactable layer

---
+++ BOX-BUX/Assets/Scripts/Holder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using VHS;
+
+public class Holder : MonoBehaviour
+{
+    public Pickable held;
+    public bool releasing;
+    public Vector3 holdOffset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        releasing = false;
+        if (held == null)
+        {
+            return;
+        }
+        var pos = transform.position;
+        var ray = new Ray(new Vector3(pos.x + holdOffset.x, pos.y + holdOffset.y, pos.z + holdOffset.z), transform.forward);
+
+        held.transform.position = ray.GetPoint(2);
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Releasing");
+            held.rigid.isKinematic = false;
+            held = null;
+            releasing = true;
+        }
+    }
+
+    public void SetHeld(Pickable held)
+    {
+        this.held = held;
+        held.rigid.isKinematic = true;
+    }
+}
---
+++ BOX-BUX/Assets/Scripts/Interactables/Pickable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VHS
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class Pickable : InteractableBase
+    {
+        public Holder holder;
+        public Rigidbody rigid;
+
+        private void Awake()
+        {
+            holder = FindObjectOfType<Holder>();
+            rigid = GetComponent<Rigidbody>();
+        }
+
+        public override void OnInteract()
+        {
+            if (holder.held != gameObject && holder.releasing == false)
+            {
+                Debug.Log("Holding");
+                holder.SetHeld(this);
+            }
+        }
+
+        public void OnHold()
+        {
+            Debug.Log("onhold");
+
+        }
+
+        public void OnPickUp()
+        {
+            Debug.Log("OnPickUp");
+        }
+
+        public void OnRelease()
+        {
+            
+        }
+    }
+}
