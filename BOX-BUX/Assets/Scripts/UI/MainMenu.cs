5880353f8e9279436305f26cbe062c3debbc45f6
Stefan Lopuszanski <Stexe@UConn.edu>
2020-07-11 12:32:36 -0400

    Basic Title Scene and Build Settings

---
+++ BOX-BUX/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject titlePanel;
+    public GameObject selectionPanel;
+    public GameObject titleBackground;
+
+    public void StartGame()
+    {
+        titlePanel.SetActive(false);
+        selectionPanel.SetActive(false);
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Game quit!");
+        Application.Quit();
+    }
+}
---
+++ BOX-BUX/Assets/Scripts/UI/Pause.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour {
+
+	public GameObject pausePanel;
+	public static bool isPaused;
+
+	void Update ()
+    {
+        /*
+        if (Input.GetButtonDown ("Cancel") && !isPaused) 
+		{
+			DoPause();
+		}
+		else if (Input.GetButtonDown ("Cancel") && isPaused) 
+		{
+			UnPause ();
+		}
+        */
+	}
+
+	public void DoPause()
+	{
+		isPaused = true;
+		Time.timeScale = 0.0f;
+        pausePanel.SetActive(true);
+    }
+
+	public void UnPause()
+	{
+		isPaused = false;
+		Time.timeScale = 1.0f;
+        pausePanel.SetActive(false);
+	}
+
+    public void LoadTitle()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("Main");
+        Application.Quit();
+    }
+
+}
